85576f1755ab6c96e95b1f1d9970e5adafe1205d
Star122013 <hyy122013@outlook.com>
2023-12-02 22:53:04 +0800

    fix

--- home/ags/default.nix
+++ home/ags/default.nix
@@ -1,15 +1,3 @@
-{ inputs, pkgs, ... }:
-{
-  # add the home manager module
-  imports = [ inputs.ags.homeManagerModules.default ];
-
-  programs.ags = {
-    enable = true;
-
-    # null or path, leave as null if you don't want hm to manage the config
-    configDir = ./ags;
-
-    # packages to add to gjs's runtime
-    extraPackages = [ pkgs.libsoup_3 ];
-  };
+{...}:{
+    imports = [./ags];
 }
